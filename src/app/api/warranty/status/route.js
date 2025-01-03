95bef151560eca00756853e57fb376a8c3b69675
aakash <mahajanaakash108@gmail.com>
2025-01-03 20:45:52 +0530

    bug: Warranty status not working

--- src/app/api/warranty/status/route.js
+++ src/app/api/warranty/status/route.js
@@ -9,7 +9,7 @@ export async function GET(request) {
 
   try {
     client = await MongoClient.connect(process.env.MONGODB_URI);
-    const db = client.db(process.env.MONGODB_DB);
+    const db = client.db(camio-ppf);
     const warrantyCollection = db.collection('warranties');
 
     const warranty = await warrantyCollection.findOne({ warrantyId });
