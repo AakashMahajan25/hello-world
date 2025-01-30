1613ca401adb940736a56c8634688f704b805ae6
kunal89204 <kunalkhandelwal108@gmail.com>
2025-01-30 21:50:13 +0530

    changes done

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import Button2 from "../props/Button2";
 import Link from "next/link";
-import roll from '@/assets/roll.png'
+import roll from '@/assets/packaging.jpg'
 import car1 from "../../assets/herocar.png";
 import car2 from "../../assets/herocar2.png";
 import car3 from "../../assets/herocar3.png";
@@ -95,9 +95,9 @@ const Hero = () => {
           spacing={{ md: 8 }}
           gap={4}
         >
-          {/* <Box>
-            <Image src={roll} width={1000} height={1000} className="h-[450px] w-[120px] hidden md:block" />
-          </Box> */}
+          <Box>
+            <Image src={roll} width={1000} height={1000} className="h-[450px] w-[580px] hidden md:block" />
+          </Box>
           {/* Image Section */}
           <Box
             w="full"
